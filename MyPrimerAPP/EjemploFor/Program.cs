0e5c3316ac27f898677eecc2d19067da68592a66
hmansilla2019 <hmansilla2019@gmail.com>
2023-08-29 22:08:55 -0300

    Clase 3

---
+++ MyPrimerAPP/EjemploFor/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int i;
+int cantidadNotasACargar = 0;
+double promedioNotas = 0;
+double nota;
+double sumaNotas=0;
+int contadorAprobado = 0;
+int contadorReprobado = 0;
+bool aplazado = false;
+
+Console.WriteLine("Ingrese cantidad de notas a cargar");
+cantidadNotasACargar = int.Parse(Console.ReadLine());
+
+for (i = 1; i <= cantidadNotasACargar; i++)
+{
+    Console.WriteLine("Ingrese la nota:");
+    nota = double.Parse(Console.ReadLine());
+    if (nota >=7)
+    {
+        contadorAprobado++;
+    }
+    else
+    {
+        if (nota<=2)
+        {
+            aplazado = true;
+            break;
+        }
+        if (nota>2 && nota <4)
+        {
+            cantidadNotasACargar--;
+            continue;
+        }
+        contadorReprobado++;
+
+    }
+
+    sumaNotas += nota;
+
+}
+if (aplazado)
+{
+    Console.WriteLine("Rinde Final");
+}
+promedioNotas = sumaNotas / cantidadNotasACargar;
+Console.WriteLine($"El promedio de las notas: {promedioNotas}");
+Console.WriteLine($"Aprobraron: {contadorAprobado}");
+Console.WriteLine($"Reprobarom: {contadorReprobado}");
+Console.ReadKey();
---
+++ MyPrimerAPP/FuncionesCadena/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string myCadena = "Bienvenido Hugo";
+string cadenaVacia = string.Empty;
+string cadenaNula = null;
+string cadenaNombre = "Hugo";
+int edad = 40;
+bool estado = true;
+
+
+if (cadenaNula == cadenaVacia)
+{
+    Console.WriteLine("Error");
+}
+
+Console.WriteLine(myCadena.Length);
+Console.WriteLine(cadenaVacia.Length);
+Console.WriteLine(myCadena.ToLower());
+Console.WriteLine(myCadena.ToUpper());
+
+if (myCadena.Contains(cadenaNombre))
+{
+    Console.WriteLine(myCadena);
+}
+
+if (cadenaNombre.Equals("Juan"))
+{
+    Console.WriteLine("Bienvenido Juan");
+}
+else
+{
+    Console.WriteLine(myCadena);
+}
+
+Console.WriteLine(edad.ToString() + estado.ToString());
+
+Console.ReadKey();
+
+
